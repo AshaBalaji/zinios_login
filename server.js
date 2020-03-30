c5c93ab7b8b51351fde107737ec615101a1a4e99
Asha <ashalahari124@gmail.com>
2020-03-30 10:16:49 +0530

    created index html

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express=require('express')
-//const bcrypt=require('bcrypt')
+const bcrypt=require('bcrypt')
 const app= express()
-//const user=[]
-app.set('view-engine','ejs')  
-//app.use(express.urlencoded({extended:false}))
+const users=[]
+app.set('view-engine','ejs')
+app.use(express.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
 
@@ -21,8 +21,22 @@ app.post('/login',(req,res)=>{
 app.get('/reg',(req,res)=>{
     res.render('reg.ejs')
 })
-app.post('/reg',(req,res)=>{
-    //req.body.name
+app.post('/reg',async(req,res)=>{
+ try{
+    const hashedPassword = await bcrypt.hash(req.body.password,10)
+    users.push({
+        id:Date.now().toString(),
+        name:req.body.name,
+        email:req.body.email,
+        password:hashedPassword
+
+    })
+    console.log(users)
+    res.redirect('/login')
+ } catch{
+    res.redirect('/reg')
+ }
+    console.log(users);
     
 })
 
